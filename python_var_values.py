9ee2e63eead6d41db2bb0b56f3dd29eeeea760ba
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-02-18 14:12:40 -0500

    recent additions

---
+++ python_var_values.py
@@ -0,0 +1,16 @@
+# meal_completed = True
+# sub_total = 100
+# tip = sub_total * 1/5
+# total = sub_total + tip
+# receipt = "Your total is" + str(total)
+# print(receipt)
+
+first = 'Springer'
+second = 'Mantle'
+third = 'Jeter'
+
+print(first)
+
+first = third
+
+print(first)
