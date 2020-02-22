3cb0065881255095eb53c648f2c032f27155a0ea
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-02-22 16:46:07 -0500

    format method strings

---
+++ format_str_interpolation_method.py
@@ -0,0 +1,7 @@
+name = 'Amber'
+age = 19
+product = 'Python eLearning course'
+
+greeting = "Hi {0}, you are listed as {1} years old and you have purchased: {2}...".format(name, age, product)
+
+print(greeting)
