3eff66d3e37596e42c39251e19a7a5595ed4a220
ThomasDiGregorio <digregoriothomas@yahoo.com>
2020-02-23 17:22:57 -0500

    Manual Exponent Function

---
+++ manual_exponent_function.py
@@ -0,0 +1,26 @@
+from functools import reduce
+
+"""
+manual_exponent (2, 3)
+#> 8
+
+manual_exponent (10, 2)
+#> 100
+"""
+
+def manual_exponent (num, exp):
+    computed_list = [num] * exp 
+    return (reduce(lambda total, element: total * element, computed_list))
+
+
+
+
+
+
+
+
+
+
+
+print(manual_exponent(2, 3))
+print(manual_exponent(10, 2))
